86027f2984c104303cf7fc15995f5d5beea03ae3
MarkoSchoretits <marko@schoretits.com>
2023-02-24 15:01:57 +0100

    U show dish details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { dishes } from '../dishes';
+import { IDishes } from '../IDishes';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+
+dish: IDishes = {} as IDishes;
+id: number = 0;
+
+constructor(private route: ActivatedRoute) { }
+
+ngOnInit(): void {
+  this.route.params.subscribe((params: Params) => {
+    this.id = +params['dishId'];
+    this.dish = dishes[this.id];
+  });
+}
 
 }

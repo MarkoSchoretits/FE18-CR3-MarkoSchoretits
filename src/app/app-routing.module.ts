49b6ebb2e41990bd7da7212055038cb6238bc690
MarkoSchoretits <marko@schoretits.com>
2023-02-24 13:29:06 +0100

    U output menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component: HomeComponent
+  },
+  {
+    path:"about", component: AboutUsComponent
+  },
+  {
+    path:"menu", component: MenuComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { dishes } from '../dishes';
+import { IDishes } from '../IDishes';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  dishes:IDishes[] = dishes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
 
 }

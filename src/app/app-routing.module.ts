a3e64959dbfa07dfb3a2b9eb4bf53dda8159746b
MarkoSchoretits <marko@schoretits.com>
2023-02-24 14:34:41 +0100

    U add link for details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 
@@ -13,6 +14,9 @@ const routes: Routes = [
   },
   {
     path:"menu", component: MenuComponent
+  },
+  {
+    path: "details/:dishId", component: DetailsComponent
   }
 ];
 

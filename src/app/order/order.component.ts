dc40907e89013c5b75f9d0d0fcf5a85bc2d15ac2
MarkoSchoretits <marko@schoretits.com>
2023-02-25 00:32:19 +0100

    U add show Order list

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { IDishes } from '../IDishes';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent {
+
+export class OrderComponent implements OnInit {
+  items: IDishes[] = [];
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.items = this.cartService.getItems();
+  }
 
 }
